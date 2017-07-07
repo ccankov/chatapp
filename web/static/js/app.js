7441d4dc367913e7e3ead75355a4ddadebb4dccb
Chris Cankov <cankov.chris@gmail.com>
2017-07-06 18:36:35 -0700

    Add user list tracking on front end

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -18,6 +18,43 @@ let user = document.getElementById("User").innerText
 let socket = new Socket("/socket", {params: {user: user}})
 socket.connect()
 
+let presences = {}
+let formatTimestamp = (timestamp) => {
+  let date = new Date(timestamp)
+  return date.toLocaleTimeString()
+}
+let listBy = (user, {metas: metas}) => {
+  return {
+    user: user,
+    onlineAt: formatTimestamp(metas[0].online_at)
+  }
+}
+
+let userList = document.getElementById("UserList")
+let render = (presences) => {
+  userList.innerHTML = Presence.list(presences, listBy)
+    .map(presence => `
+      <li>
+        ${presence.user}
+        <br>
+        <small>online since ${presence.onlineAt}</small>
+      </li>
+    `).join("")
+}
+
+// Channels
+let room = socket.channel("room:lobby")
+room.on("presence_state", state => {
+  presences = Presence.syncState(presences, state)
+  render(presences)
+})
+
+room.on("presence_diff", diff => {
+  presences = Presence.syncDiff(presences, diff)
+  render(presences)
+})
+
+room.join()
 // Import local files
 //
 // Local files can be imported directly using relative
